b921eb4c81cd82c6c78d7906e9a2a637904e729e
hsanmartin <hory.san93@gmail.com>
2022-05-07 00:50:21 -0400

    Actualización pagina inicio

--- blogdehoryan/blogdehoryan/urls.py
+++ blogdehoryan/blogdehoryan/urls.py
@@ -16,7 +16,10 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 
+from .views import inicio
+
 urlpatterns = [
+    path('', inicio),
     path('admin/', admin.site.urls),
     path('App/', include('App.urls')),
 
